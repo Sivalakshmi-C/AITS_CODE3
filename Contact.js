5e14d44d78f66cc60d91c4c6aa91d4998cf8df2f
Sivalakshmi-C <csivalakshmi9@gmail.com>
2024-07-05 10:50:15 +0530

    Add files via upload

---
+++ Contact.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Contact = () => {
+    return ( 
+        <div>
+           <h1>contact page</h1>
+        </div>
+     );
+}
+ 
+export default Contact;
---
+++ Notfound.js
@@ -0,0 +1,11 @@
+import react from 'react';
+
+const Notfound = () => {
+    return ( 
+        <div>
+            <h1>Notfound</h1>
+        </div>
+     );
+}
+ 
+export default Notfound;
